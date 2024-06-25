5471a5c22049013dbe82828a1fcbab90a9e0d054
michal <michal.rutowicz94@gmail.com>
2024-06-25 14:28:32 +0200

    2st step of library project

--- src/main/java/libraryProject/Library.java
+++ src/main/java/libraryProject/Library.java
@@ -1,4 +1,16 @@
 package libraryProject;
 
-public class Library {
+public class Library
+{
+    public static void main(String[] args) {
+
+        Book book1 = new Book();
+        Book book2 = new Book();
+
+
+        book1.title = "Ogniem i Mieczem";
+        book1.author = "Henryk Sienkiewicz";
+
+        System.out.println("Wyswietl tytul i autora: " + book1.title+ " " +book1.author);
+    }
 }
